ab26d3a0742cf7e9d716e990f92c1f0fc34a9680
simmarith <menzeladrian@googlemail.com>
2017-06-24 13:47:13 +0200

    First Files

---
+++ Assets/DestinationSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestinationSelect : MonoBehaviour {
+
+    public AudioClip changeNotification;
+    private AudioSource mainAudio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ButtonClicked()
+    {
+        Debug.Log("Ziel wurde ausgewählt!");
+        SceneManager.LoadScene("FotosphereMono");
+    }
+}
---
+++ Assets/TestJsonToUnity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using DotNetLibrary.Test1;
+
+public class TestJsonToUnity : MonoBehaviour {
+    public string aTest;
+	// Use this for initialization
+	void Start () {
+        //aTest = DotNetLibrary.Test1.Test;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
